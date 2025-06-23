c7f736b41253532d5e6fb800242c1faba5b1f958
Conchero <florian.parra@hotmail.com>
2025-06-23 17:03:20 +0200

    la la la la la

--- src/DataFixtures/RoomFixtures.php
+++ src/DataFixtures/RoomFixtures.php
@@ -7,35 +7,54 @@ use App\Entity\Room;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\Collection;
+
 
 
 class RoomFixtures extends Fixture implements DependentFixtureInterface
 {
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-for ($i = 0; $i < 20; $i++) {
-    /** @var \App\Entity\User $owner */
-$owner = $this->getReference('user-' . $faker->numberBetween(0, 9), \App\Entity\User::class);
+        for ($i = 0; $i < 20; $i++) {
+            /** @var \App\Entity\User $owner */
+            $owner = $this->getReference('user-' . $faker->numberBetween(0, 9), \App\Entity\User::class);
 
 
-    $room = new Room();
-    $room->setTitle($faker->company . ' Salle')
-        ->setSlug('salle-' . $i)
-        ->setImage('default.jpg')
-        ->setLocalisation($faker->address())
-        ->setKeywords(implode(', ', $faker->words(5)))
-        ->setDescription($faker->paragraph())
-        ->setIsAvailable($faker->boolean(90))
-        ->setCapacity($faker->numberBetween(10, 200))
-        ->setOwner($owner)
-    ;
+            $room = new Room();
+            $room->setTitle($faker->company . ' Salle')
+                ->setSlug('salle-' . $i)
+                ->setImage('default.jpg')
+                ->setLocalisation($faker->address())
+                ->setKeywords(implode(', ', $faker->words(5)))
+                ->setDescription($faker->paragraph())
+                ->setIsAvailable($faker->boolean(90))
+                ->setCapacity($faker->numberBetween(10, 200))
+                ->setOwner($owner)
+                ->addAdvantage($this->getReference('advantage-' . $faker->numberBetween(0, 3), \App\Entity\Advantage::class))
+                ->addAdvantage($this->getReference('advantage-' . $faker->numberBetween(0, 3), \App\Entity\Advantage::class))
+                ->addAdvantage($this->getReference('advantage-' . $faker->numberBetween(0, 3), \App\Entity\Advantage::class))
+                ->addEquipment($this->getReference('equipment-' . $faker->numberBetween(0, 3), \App\Entity\Equipment::class))
+                ->addEquipment($this->getReference('equipment-' . $faker->numberBetween(0, 3), \App\Entity\Equipment::class))
+                ->addEquipment($this->getReference('equipment-' . $faker->numberBetween(0, 3), \App\Entity\Equipment::class))
+                ->addSoftware($this->getReference('software-' . $faker->numberBetween(0, 3), \App\Entity\Software::class))
+                ->addSoftware($this->getReference('software-' . $faker->numberBetween(0, 3), \App\Entity\Software::class))
+                ->addSoftware($this->getReference('software-' . $faker->numberBetween(0, 3), \App\Entity\Software::class));
 
-$manager->persist($room);
-$this->addReference('room-' . $i, $room);
 
-}
+
+            // for ($i = 0; $i<4;$i++)
+            // {
+            //     $room->addAdvantage($this->getReference('advantage-' . $faker->numberBetween(0, 3), \App\Entity\Advantage::class))
+            //     ->addEquipment($this->getReference('equipment-' . $faker->numberBetween(0, 3), \App\Entity\Equipment::class))
+            //     ->addSoftware($this->getReference('software-' . $faker->numberBetween(0, 3), \App\Entity\Software::class));
+            // }
+
+            $manager->persist($room);
+            $this->addReference('room-' . $i, $room);
+        }
 
 
 
