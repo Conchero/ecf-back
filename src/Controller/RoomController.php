893be7514fe62f6dad222e275612965ddf7c0806
soumia <soumya.ould@gmail.com>
2025-06-24 12:50:42 +0200

    creation new, supprimer, et modif + barre recherche

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -45,6 +45,9 @@ final class RoomController extends AbstractController
                 $room->setImage($uploadService->upload($image, 'image'));
             }
 
+            
+            $room->setIsAvailable(true); 
+
             $this->em->persist($room);
             $this->em->flush();
 
@@ -66,8 +69,78 @@ final class RoomController extends AbstractController
             throw $this->createNotFoundException('Salle non trouvée.');
         }
 
+        if (!$room->isAvailable()) {
+            if ($room->getOwner() !== $this->getUser()) {
+                $this->addFlash('danger', "La salle n'est pas accessible pour le moment.");
+                return $this->redirectToRoute('app_room');
+            }
+        }
+
         return $this->render('room/view.html.twig', [
-            'room' => $room,
+            'room' => $room
         ]);
+    }
+
+    #[Route('/room/{slug}/edit', name: 'room_edit', methods: ['GET', 'POST'])]
+    public function edit(string $slug, Request $request, UploadService $us): Response
+    {
+        $room = $this->em->getRepository(Room::class)->findOneBy(['slug' => $slug]);
+
+        if (!$room) {
+            $this->addFlash('danger', "La salle n'existe pas");
+            return $this->redirectToRoute('app_room');
+        }
+
+        $form = $this->createForm(RoomForm::class, $room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                if ($image = $form->get('image')->getData()) {
+                    $us->delete($room->getImage(), 'image');
+                    $room->setImage($us->upload($image, 'image'));
+                }
+
+                if ($slugEdit = $request->get('slug-edit')) {
+                    $slugify = new Slugify();
+                    if ($room->getSlug() !== $slugEdit) {
+                        $room->setSlug($slugify->slugify($slugEdit));
+                    } else {
+                        $room->setSlug($slugify->slugify($room->getTitle()));
+                    }
+                }
+
+                $this->em->persist($room);
+                $this->em->flush();
+
+                $this->addFlash('success', 'Modification bien prise en compte');
+            } catch (\Throwable $th) {
+                $this->addFlash('danger', 'La modification a rencontré une erreur');
+            }
+
+            return $this->redirectToRoute('room_view', ['slug' => $room->getSlug()]);
+        }
+
+        return $this->render('room/edit.html.twig', [
+            'roomForm' => $form,
+            'room' => $room
+        ]);
+    }
+
+    #[Route('/room/{slug}/delete', name: 'room_delete', methods: ['POST'])]
+    public function delete(string $slug): Response
+    {
+        $room = $this->em->getRepository(Room::class)->findOneBy(['slug' => $slug]);
+
+        if (!$room) {
+            $this->addFlash('danger', "La salle n'existe pas");
+            return $this->redirectToRoute('app_room');
+        }
+
+        $this->em->remove($room);
+        $this->em->flush();
+
+        $this->addFlash('success', "Salle supprimée avec succès");
+        return $this->redirectToRoute('app_room');
     }
 }
