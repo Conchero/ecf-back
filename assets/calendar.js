ba379bb8689f0b35cc73cfdfd55bbeff74e875dc
Conchero <florian.parra@hotmail.com>
2025-06-25 16:30:44 +0200

    first calendar part

--- assets/calendar.js
+++ assets/calendar.js
@@ -1,17 +1,55 @@
-// // commonjs
-// const flatpickr = require("flatpickr");
-
-// es modules are recommended, if available, especially for typescript
-import flatpickr from "flatpickr";
 
 
-const reservationStart = document.querySelector(".reservation-date.start");
 
-// If using flatpickr in a framework, its recommended to pass the element directly
-flatpickr(reservationStart, {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i",
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentErrorMessage = null;
+    const inputs = document.querySelectorAll(".reservation");
+    const errorMessage = document.querySelectorAll(".error-message");
+
+    const errorMessageArray = [];
+    
+    const ErrorMessageToDOM = ((el,remove) => {
+        if (errorMessageArray.length> 0 && errorMessageArray.find(el) === remove)
+        {
+          remove ? errorMessageArray.remove(el) : errorMessageArray.push(el);
+        }
+    })
+
+    const compareToExistingReservation = (stringToCompare => {
+
+        errorMessage.forEach(el => {
+            console.log(el);
+            const dateToCompare = el.querySelectorAll(".reservation-date");
+
+
+            let reservationArray = {
+                start: "",
+                end: ""
+            }
+
+
+            dateToCompare.forEach(date => {
+                reservationArray.start = date.getAttribute("data-reservation-start").split("-").join("")
+                reservationArray.end = date.getAttribute("data-reservation-end").split("-").join("")
+            })
+
+            console.log(reservationArray);
+
+            const inputToCompare = parseInt(stringToCompare.split("-").join(""));
+            const start = parseInt(reservationArray.start);
+            const end = parseInt(reservationArray.end);
+
+        })
+    })
+
+
+
+    inputs.forEach(el => {
+        el.addEventListener("change", (e) => {
+            compareToExistingReservation(e.target.value);
+        })
+    })
+
 });
-
-
-console.log(reservationStart);
--- importmap.php
+++ importmap.php
@@ -16,7 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-
+    'calendar' => [
+        'path' => './assets/calendar.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
